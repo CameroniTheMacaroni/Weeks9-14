b5af1acd1095d75f107803769c3caf79944deba9
Cameron Sensicle <sensicle@shernet.sheridancollege.ca>
2026-03-24 11:17:09 -0400

    Started working on systems project - making tiles

---
+++ Assets/Scripts/SystemProject/TileScript.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class TileScript : MonoBehaviour
+{
+    public int color; //decides what color the tile will be, 1-red, 2-blue, 3-yellow, 4-green, 5-orange, 6-purple
+    public SpriteRenderer sprtRen;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+    public void changeColor()
+    {
+
+    }
+}
