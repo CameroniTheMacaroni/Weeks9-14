7b93a880e9fc65c4f6de733ed4d5281d73e3426c
Cameron Sensicle <sensicle@shernet.sheridancollege.ca>
2026-03-24 09:38:12 -0400

    added a running animation to the knight with animation events to make footstep noises

---
+++ Assets/Scripts/Week11Class/Knight.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Knight : MonoBehaviour
+{
+    public AudioSource sound;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+    public void footstep()
+    {
+        sound.Play();
+    }
+}
