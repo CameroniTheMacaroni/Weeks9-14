074b5ca9e2d1d46316acedeaa7f756a2d453022c
Cameron Sensicle <sensicle@shernet.sheridancollege.ca>
2026-03-25 16:05:40 -0400

    made random colors

--- Assets/Scripts/SystemProject/TileSpawnerScript.cs
+++ Assets/Scripts/SystemProject/TileSpawnerScript.cs
@@ -1,12 +1,17 @@
 using NUnit.Framework;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TileSpawnerScript : MonoBehaviour
 {
     public GameObject tile;
+    public GameObject spawnedTile;
     public List<GameObject> tiles;
+    public TileScript spawnedScript;
+
+    public int randomNumber;
 
     public Vector2 addToPositionX = new Vector2(2, 0);
     public Vector2 addToPositionY = new Vector2(0, -2);
@@ -25,10 +30,13 @@ public class TileSpawnerScript : MonoBehaviour
         transform.position = new Vector3(-9, 4, 0);//start at the top left
         for (int i = 0; i <= 10; i++)//go row by row (vertical)
         {
-            Instantiate(tile, transform.position, transform.rotation);//spawn a tile at the current position
             for (int j = 0; j <= 5; j++)//go column by column (horizontal)
             {
-                Instantiate(tile, transform.position, transform.rotation);//spawn a tile at the current position
+                randomNumber = Random.Range(1, 7);//select a random number between 1-6
+                spawnedTile = Instantiate(tile, transform.position, transform.rotation);//spawn a tile at the current position
+                spawnedTile.GetComponent<TileScript>().color = randomNumber;//set the color to something random
+                spawnedTile.GetComponent<TileScript>().changeColor();//update the tiles to reflect that color
+
                 transform.position += (Vector3)addToPositionY;//move down
             }
             transform.position = new Vector3(transform.position.x, 4, 0);//go back to the top
