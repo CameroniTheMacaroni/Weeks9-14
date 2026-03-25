c9f2852a850baec43ca05636ee9950acb889837e
Cameron Sensicle <sensicle@shernet.sheridancollege.ca>
2026-03-25 17:09:01 -0400

    Done spawning the tiles

--- Assets/Scripts/SystemProject/TileSpawnerScript.cs
+++ Assets/Scripts/SystemProject/TileSpawnerScript.cs
@@ -27,20 +27,42 @@ public class TileSpawnerScript : MonoBehaviour
     }
     public void spawnTiles()
     {
+        StartCoroutine(animateTilesSpawn());
+    }
+    public void clearTiles()
+    {
+        StartCoroutine(animateTilesClear(tiles.Count));
+    }
+    IEnumerator animateTilesSpawn()
+    {
         transform.position = new Vector3(-9, 4, 0);//start at the top left
-        for (int i = 0; i <= 10; i++)//go row by row (vertical)
+        for (int i = 0; i <= 9; i++)//go row by row (vertical)
         {
-            for (int j = 0; j <= 5; j++)//go column by column (horizontal)
+            for (int j = 0; j <= 4; j++)//go column by column (horizontal)
             {
                 randomNumber = Random.Range(1, 7);//select a random number between 1-6
                 spawnedTile = Instantiate(tile, transform.position, transform.rotation);//spawn a tile at the current position
                 spawnedTile.GetComponent<TileScript>().color = randomNumber;//set the color to something random
                 spawnedTile.GetComponent<TileScript>().changeColor();//update the tiles to reflect that color
 
+                tiles.Add(spawnedTile);
+
                 transform.position += (Vector3)addToPositionY;//move down
+                yield return new WaitForSeconds(0.05f);
             }
             transform.position = new Vector3(transform.position.x, 4, 0);//go back to the top
-            transform.position += (Vector3) addToPositionX;//move right
+            transform.position += (Vector3)addToPositionX;//move right
+            yield return new WaitForSeconds(0.05f);
+
+        }
+    }
+    IEnumerator animateTilesClear(int length)
+    {
+        for (int i = 0; i < length; i++)
+        {
+            Destroy(tiles[0]);
+            tiles.Remove(tiles[0]);
+            yield return new WaitForSeconds(0.05f);
         }
     }
 }
